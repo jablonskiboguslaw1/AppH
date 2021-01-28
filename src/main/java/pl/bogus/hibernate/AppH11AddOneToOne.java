e2da259a2fc4e3210c497b860b3b1d68f1941ab7
Bogus <jablonski.boguslaw@gmail.com>
2021-01-28 18:26:41 +0100

    update entities with OneToOne ralationship

--- src/main/java/pl/bogus/hibernate/AppH11AddOneToOne.java
+++ src/main/java/pl/bogus/hibernate/AppH11AddOneToOne.java
@@ -2,8 +2,8 @@ package pl.bogus.hibernate;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import pl.bogus.hibernate.entity.Attribute;
+import pl.bogus.hibernate.entity.Category;
 import pl.bogus.hibernate.entity.Product;
 
 import javax.persistence.EntityManager;
@@ -11,7 +11,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 
-public class AppH10ManyToMany {
+public class AppH11AddOneToOne {
     private static final Logger logger = LogManager.getLogger(AppH.class);
     private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
 
@@ -20,17 +20,27 @@ public class AppH10ManyToMany {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         entityManager.getTransaction().begin();
 
+
+
+
         Product product = entityManager.find(Product.class, 3L);
 
-        logger.info(product);
+        Category category = new Category();
+        category.setName("newCategory");
+        category.setDescription("This is new Category");
+        product.setCategory(category);
+        entityManager.persist(category);
 
-        logger.info(product.getAttributes());
 
-        logger.info("===========================");
-Attribute attribute = entityManager.find(Attribute.class,1L);
-logger.info(attribute);
-logger.info(attribute.getProducts());
 
+
+/*
+
+        //można pobrać kategorię i  ją przypisać ale jeśli tworzymy nową (new) to jest w stanie TRANSIENT i trzeba ją najpierw zapisać
+        Category category = entityManager.find(Category.class, 2L);
+*/
+
+        product.setCategory(category);
 
         entityManager.getTransaction().commit();
         entityManager.close();
